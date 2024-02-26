ab713409988378b70d4e2eb34e659f74ac0a674d
TuBT <buitu3@gmail.com>
2024-02-26 17:38:35 +0700

    - import hidden object textures - add hidden prob scriptable object

---
+++ Assets/Scripts/HiddenProb.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HiddenObject
+{
+    public class HiddenProb : MonoBehaviour
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObjects/HiddenProbSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HiddenObject
+{
+    [CreateAssetMenu(fileName = "HiddenProbData", menuName = "ScriptableObjects/CreateProbDataSO", order = 1)]
+    public class HiddenProbSO : ScriptableObject
+    {
+        
+    }
+}
